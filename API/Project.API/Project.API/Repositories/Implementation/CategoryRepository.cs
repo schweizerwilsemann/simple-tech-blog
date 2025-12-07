af865e5028ff322c51eb0946b8368c98b212bacf
KhoaND <khoanguyenduc99@gmail.com>
2025-12-07 10:12:11 +0700

    add category repository and its interface

---
+++ API/Project.API/Project.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Project.API.Data;
+using Project.API.Models.Domain;
+using Project.API.Repositories.Interface;
+
+namespace Project.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await _dbContext.Categories.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
---
+++ API/Project.API/Project.API/Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Project.API.Models.Domain;
+
+namespace Project.API.Repositories.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateAsync(Category category);
+    }
+}
