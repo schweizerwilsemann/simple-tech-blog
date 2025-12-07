77e46c92dd444efd675b31fe38d96cd7fab93d0a
KhoaND <khoanguyenduc99@gmail.com>
2025-12-07 10:10:41 +0700

    add ApplicationDbContext, BlogPost, and Category models

---
+++ API/Project.API/Project.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.API.Models.Domain;
+
+namespace Project.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
