74f7a630e989879a85ec17372cbc1d52e8fe6b82
KhoaND <khoanguyenduc99@gmail.com>
2025-12-07 10:11:14 +0700

    add CategoryDTO and CreateCategoryRequestDTO models

---
+++ API/Project.API/Project.API/Models/DTOs/CreateCategoryRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.API.Models.DTOs
+{
+    public class CreateCategoryRequestDTO
+    {
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
