535707512bc96b59dc4b70ac84b5ea0b63dd263e
KhoaND <khoanguyenduc99@gmail.com>
2025-12-07 10:10:57 +0700

    add initial migration for BlogPosts and Categories tables

---
+++ API/Project.API/Project.API/Migrations/20251207021301_add_featured_image_url.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfeaturedimageurl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FeaturedImageUrl",
+                table: "BlogPosts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FeaturedImageUrl",
+                table: "BlogPosts");
+        }
+    }
+}
