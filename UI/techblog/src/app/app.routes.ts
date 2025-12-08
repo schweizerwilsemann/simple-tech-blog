3108814a5d94c5e2ebffc5617dc8098ef2e86fe2
KhoaND <khoanguyenduc99@gmail.com>
2025-12-08 10:10:43 +0700

    (feat) add route for application

--- UI/techblog/src/app/app.routes.ts
+++ UI/techblog/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CategoryList } from './features/categories/category-list/category-list';
+import { HomeComponent } from './features/home/home';
+import { AddCategory } from './features/categories/add-category/add-category';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: "admin/categories",
+    component: CategoryList
+  },
+  {
+    path: 'admin/categories/add',
+    component: AddCategory
+  }
+];
