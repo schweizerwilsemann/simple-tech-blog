92ee8843bcfbb02ca6d0527265abd247de8e4b06
KhoaND <khoanguyenduc99@gmail.com>
2025-12-08 10:12:47 +0700

    feat: add category list component with template, styles, and tests

---
+++ UI/techblog/src/app/features/categories/category-list/category-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-category-list',
+  imports: [RouterLink],
+  templateUrl: './category-list.html',
+  styleUrl: './category-list.scss',
+})
+export class CategoryList {
+
+}
