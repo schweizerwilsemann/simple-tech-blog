d5201c6cd74845da7f2047e470530b3119dfb6f7
KhoaND <khoanguyenduc99@gmail.com>
2025-12-10 08:44:05 +0700

    feat: adding base of creating blogpost layout

--- UI/techblog/src/app/app.routes.ts
+++ UI/techblog/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CategoryList } from './features/categories/category-list/category-list'
 import { HomeComponent } from './features/home/home';
 import { AddCategory } from './features/categories/add-category/add-category';
 import { EditCategory } from './features/categories/edit-category/edit-category';
+import { BlogpostList } from './features/blog-post/blogpost-list/blogpost-list';
 
 export const routes: Routes = [
   {
@@ -20,5 +21,9 @@ export const routes: Routes = [
   {
     path: 'admin/categories/:id',
     component: EditCategory
+  },
+  {
+    path: 'admin/blogposts',
+    component: BlogpostList
   }
 ];
