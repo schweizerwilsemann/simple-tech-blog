d043b9054952344bd293d52a3b02020e7db6bb97
KhoaND <khoanguyenduc99@gmail.com>
2025-12-11 09:30:50 +0700

    feat: add to choose categories in creating post

--- UI/techblog/src/app/features/blog-post/add-blogpost/add-blogpost.ts
+++ UI/techblog/src/app/features/blog-post/add-blogpost/add-blogpost.ts
@@ -1,21 +1,27 @@
 import { AddBlogPost } from '@/app/features/blog-post/models/add-blogpost.model';
-import { DatePipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BlogPostService } from '@/app/features/blog-post/services/blog-post';
 import { Router } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
+import { CategoryService } from '@/app/features/categories/services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '@/app/features/categories/models/category.model';
 
 @Component({
   selector: 'app-add-blogpost',
-  imports: [FormsModule, ReactiveFormsModule, DatePipe, MarkdownModule],
+  imports: [FormsModule, ReactiveFormsModule, DatePipe, AsyncPipe, MarkdownModule],
   templateUrl: './add-blogpost.html',
   styleUrl: './add-blogpost.scss',
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit{
   model: AddBlogPost;
+  categories$? : Observable<Category[]>;
+
   private blogPostService = inject(BlogPostService);
   private router = inject(Router);
+  private categoryService = inject(CategoryService)
 
   constructor () {
     this.model = {
@@ -26,8 +32,12 @@ export class AddBlogpostComponent {
       urlHandle: '',
       author: '',
       publishedDate: new Date(),
-      isVisible: true
+      isVisible: true,
+      categories: []
     }
+  }
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
   }
 
   onFormSubmit = () => {
