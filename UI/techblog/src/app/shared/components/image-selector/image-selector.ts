9fc4bea27d1f3c471b18d6c1b78e44b719d988a2
KhoaND <khoanguyenduc99@gmail.com>
2025-12-12 10:29:30 +0700

    feat: create image selector component

---
+++ UI/techblog/src/app/shared/components/image-selector/image-selector.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-selector',
+  imports: [],
+  templateUrl: './image-selector.html',
+  styleUrl: './image-selector.scss',
+})
+export class ImageSelector {
+
+}
