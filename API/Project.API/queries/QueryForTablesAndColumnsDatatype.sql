a1fd6dded90e5829f7d6e67ca6d401ef9bb56ab8
KhoaND <khoanguyenduc99@gmail.com>
2025-12-16 07:38:36 +0700

    datagrip

---
+++ API/Project.API/queries/QueryForTablesAndColumnsDatatype.sql
@@ -0,0 +1,15 @@
+-- List all tables and their columns in the current database
+SELECT
+    t.TABLE_SCHEMA AS SchemaName,
+    t.TABLE_NAME AS TableName,
+    c.COLUMN_NAME AS ColumnName,
+    c.DATA_TYPE AS DataType,
+    c.CHARACTER_MAXIMUM_LENGTH AS MaxLength,
+    c.IS_NULLABLE AS IsNullable,
+    c.COLUMN_DEFAULT AS DefaultValue
+FROM INFORMATION_SCHEMA.TABLES t
+JOIN INFORMATION_SCHEMA.COLUMNS c
+    ON t.TABLE_SCHEMA = c.TABLE_SCHEMA
+    AND t.TABLE_NAME = c.TABLE_NAME
+WHERE t.TABLE_TYPE = 'BASE TABLE'
+ORDER BY t.TABLE_SCHEMA, t.TABLE_NAME, c.ORDINAL_POSITION;
